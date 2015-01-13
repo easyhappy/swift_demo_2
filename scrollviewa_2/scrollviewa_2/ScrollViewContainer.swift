26f1c3700b414bde7226fef54c616d95caa4124c
AndyHu <meeasyhappy@gmail.com>
2015-01-13 18:40:17 +0800

    No message.

---
+++ scrollviewa_2/scrollviewa_2/ScrollViewContainer.swift
@@ -0,0 +1,16 @@
+    import UIKit
+    
+    class ScrollViewContainer: UIView {
+        @IBOutlet var scrollView: UIScrollView!
+        
+        override func hitTest(point: CGPoint, withEvent event: UIEvent!) -> UIView? {
+            let view = super.hitTest(point, withEvent: event)?
+            if let theView = view {
+                if theView == self {
+                    return scrollView
+                }
+            }
+            
+            return view
+        }
+    }
